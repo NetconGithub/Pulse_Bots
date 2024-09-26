96da3fba7fe809c7fb20869d72fc725c3beb7930
NCMPAutomation <netconncmp@netcon.in>
2024-09-26 17:16:35 +0530

    Add files via upload

---
+++ File_Operation/File_Operation_Copy_file.ps1
@@ -0,0 +1,5 @@
+﻿#************************** Copy file from one path to another path ***************************************#
+
+$path1="Source Path of the file"
+$path2="Destination Path of the file"
+Copy-Item $path1 -Destination $path2
