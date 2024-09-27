f91a366fb0bfc23085734e30e0b28d10196db0a3
NCMPAutomation <netconncmp@netcon.in>
2024-09-27 12:39:12 +0530

    Add files via upload

---
+++ XML_Operations/XML_Operations_Modify_XML_Attributes.ps1
@@ -0,0 +1,24 @@
+﻿$path             = 'C:\Data.xml'
+$ManagementServer = 'NewManagementServer'
+$SQLServer        = 'NewSQLServer'
+$SQLAdmin         = 'Domain\NewSQlAdmin'
+$DNSServerVMName  = 'NewDNSServer'
+
+$xml = [xml](Get-Content $path)
+
+$node = $xml.Data.Roles.Role |
+    where {$_.Name -eq 'ManagementServer'}
+$node.Value = $ManagementServer
+
+$node        = $xml.Data.SQL.Instance
+$node.Server = $SQLServer
+
+$node = $xml.Data.SQL.Instance.Variable |
+    where {$_.Name -eq 'SQLAdmin'}
+$node.Value = $SQLAdmin
+
+$node = $xml.Data.VMs.VM |
+    where {$_.Type -eq 'DNSServerVM'}
+$node.VMName = $DNSServerVMName
+
+$xml.Save($path)
