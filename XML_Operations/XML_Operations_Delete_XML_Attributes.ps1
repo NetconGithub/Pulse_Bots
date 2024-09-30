5ff83b0c1083cdd2e14cba4a154c21d205d13add
NCMPAutomation <netconncmp@netcon.in>
2024-09-30 15:53:26 +0530

    Delete XML_Operations directory

--- XML_Operations/XML_Operations_Delete_XML_Attributes.ps1
+++
@@ -1,9 +0,0 @@
-﻿$xml = [xml](Get-Content C:\Temp\SampleXML.xml)
-$node = $xml.SelectSingleNode("//book[@id='bk102']")
-$node.ParentNode.RemoveChild($node) | Out-Null
-$xml.Save('C:\Temp\SampleXML.xml')
-
-$xml = [xml](Get-Content C:\Temp\SampleXML.xml)
-$xml.SelectNodes("//book")
-$nodes = $xml.SelectNodes("//book")
-foreach($node in $nodes){$node.ParentNode.RemoveChild($node)}
--- XML_Operations/XML_Operations_Retrieve_XML_Attributes.ps1
+++
@@ -1,6 +0,0 @@
-﻿$Path = "$Pshome\Types.ps1xml"
-$XPath = "/Types/Type/Members/AliasProperty"
-$retrieve = Select-Xml -Path $Path -XPath $Xpath | Select-Object -ExpandProperty Node
-
-$retrieve
-
