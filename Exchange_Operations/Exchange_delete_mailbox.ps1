386bd8607e47f08fb1f8f68ccf40015f849f7666
NCMPAutomation <netconncmp@netcon.in>
2024-09-30 15:55:39 +0530

    Delete Exchange_Operations directory

--- Exchange_Operations/Exchange_delete_mailbox.ps1
+++
@@ -1,7 +0,0 @@
-#parameters
-$user = "Provide the user mailbox name"
-
-$delete = Get-Mailbox -Identity $user -SoftDeletedMailbox | Remove-Mailbox -PermanentlyDelete
-
-$output = Echo "$user mailbox is successfully deleted"
-
