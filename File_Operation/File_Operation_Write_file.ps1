abe0aa495e3f2ffe36675ed262afe9df8f269007
NCMPAutomation <netconncmp@netcon.in>
2024-09-30 15:52:24 +0530

    Delete File_Operation directory

--- File_Operation/File_Operation_Write_file.ps1
+++
@@ -1,6 +0,0 @@
-﻿#************************** Write data into the file ***************************************#
-
-$path="Path of the file"
-$value="Data that needsto be added"
-$data=Add-Content -Path $path  -Value "$value"
-$data
